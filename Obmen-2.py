2263d09de6ae3d820d8eb2860a79f6f979e4cdfa
Avir5 <rvbpython@gmail.com>
2024-11-20 00:09:43 +0300

    Добавил - ДОБАВИЛ-1 и ДОБАВИЛ-2

--- Obmen-2.py
+++ Obmen-2.py
@@ -13,6 +13,12 @@ def update_b_label(event):
     name = currencies[code]
     b_label.config(text=name)
 
+def update_b2_label(event):
+    # Получаем полное название второй базовой валюты из словаря и обновляем метку
+    code = b2_combobox.get()
+    name = currencies[code]
+    b2_label.config(text=name)
+
 def update_t_label(event):
     # Получаем полное название целевой валюты из словаря и обновляем метку
     code = t_combobox.get()
@@ -22,8 +28,9 @@ def update_t_label(event):
 def exchange():
     target_code = t_combobox.get()
     base_code = b_combobox.get()
+    base2_code = b2_combobox.get() #ДОБАВИЛ-1
 
-    if target_code and base_code:
+    if target_code and base_code and base2_code: #ДОБАВИЛ-2
         try:
             response = requests.get(f'https://open.er-api.com/v6/latest/{base_code}')
             response.raise_for_status()
@@ -60,24 +67,36 @@ currencies = {
 # Создание графического интерфейса
 window = Tk()
 window.title("Курс обмена валюты")
-window.geometry("360x300")
+window.geometry("360x430")
 
-Label(text="Базовая валюта:").pack(padx=10, pady=5)
+Label(text="Базовая валюта:", font='Arial 9').pack(padx=10, pady=5)
 b_combobox = ttk.Combobox(values=list(currencies.keys()))
 b_combobox.pack(padx=10, pady=5)
 b_combobox.bind("<<ComboboxSelected>>", update_b_label)
 
 b_label = ttk.Label()
-b_label.pack(padx=10, pady=10)
+b_label.pack(padx=10, pady=5)
 
-Label(text="Целевая валюта:").pack(padx=10, pady=5)
+Label(text="").pack(padx=10, pady=5)
+
+Label(text="Вторая базовая валюта:", font='Arial 9').pack(padx=10, pady=5)
+b2_combobox = ttk.Combobox(values=list(currencies.keys()))
+b2_combobox.pack(padx=10, pady=5)
+b2_combobox.bind("<<ComboboxSelected>>", update_b2_label)
+
+b2_label = ttk.Label()
+b2_label.pack(padx=10, pady=5)
+
+Label(text="").pack(padx=10, pady=5)
+
+Label(text="Целевая валюта:", font='Arial 9').pack(padx=10, pady=5)
 t_combobox = ttk.Combobox(values=list(currencies.keys()))
 t_combobox.pack(padx=10, pady=5)
 t_combobox.bind("<<ComboboxSelected>>", update_t_label)
 
 t_label = ttk.Label()
-t_label.pack(padx=10, pady=10)
+t_label.pack(padx=10, pady=5)
 
-Button(text="Получить курс обмена", command=exchange).pack(padx=10, pady=10)
+Button(text="Получить курс обмена", font='Arial 9 bold', command=exchange).pack(padx=10, pady=30)
 
 window.mainloop()
