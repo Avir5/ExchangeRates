e6202f389e4f2d2d9b1e5002772de52b4c18d926
Avir5 <rvbpython@gmail.com>
2024-11-26 02:12:47 +0300

    Близко, но ещё не закончил...

--- cryptocurrencies.py
+++ cryptocurrencies.py
@@ -3,31 +3,46 @@ from tkinter import ttk
 from tkinter import messagebox as mb
 import requests
 
-name=None
+#name=None
 
 def update_cryptocur_label(event):
+    global name
     # Получаем полное название криптовалюты из словаря и обновляем метку
     code = combobox.get()
     name = currencies[code]
     currency_label.config(text=name)
+    #name=name.lower()
+    print('Lower name:',name)
 
 def exchange():
+    #nonlocal name
     code = combobox.get()
-
     if code:
         try:
-            response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={name}&vs_currencies=usd")
+            #response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids=m{name}&vs_currencies=usd")
+            response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+            #https://api.coingecko.com/api/v3/simple/price?ids=%7Bcrypto_code%7D&vs_currencies=%7Bbase_code%7D
             #https://api.coingecko.com/api/v3/coins/list
             response.raise_for_status()
+            print('Статус:',response)
 
             data = response.json()
-
-            if name in data['ids']:
-                exchange_rate = data['ids'][code]
-                currency_name = currencies[code] # currencies.get(code, code)
+            print("data:",data)
+            print('name_after_data',name)
+            #print('Name and Code:', data[0][code])
+            #for key_data, (b, c) in enumerate(data.items()):
+            #tuple_data=data.items()
+            #print('Tuple_data:',tuple_data)
+            if name in data[name.lower().values()]:
+                print(name)
+                exchange_rate = data[name][usd]
+                print(exchange_rate)
+                currency_name = currencies[code]  # currencies.get(code, code)
+                print(currency_name)
                 mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.1f} {currency_name} за 1 доллар")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена")
+
         except Exception as e:
             mb.showerror("Ошибка", f"Ошибка: {e}")
     else:
