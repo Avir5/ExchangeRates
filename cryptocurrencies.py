8cad4e628431e1022a440279f2a851a6d6c5e488
Avir5 <rvbpython@gmail.com>
2024-11-26 15:51:32 +0300

    ПО доработано.

--- cryptocurrencies.py
+++ cryptocurrencies.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 from tkinter import messagebox as mb
 import requests
 
-#name=None
 
 def update_cryptocur_label(event):
     global name
@@ -11,38 +10,22 @@ def update_cryptocur_label(event):
     code = combobox.get()
     name = currencies[code]
     currency_label.config(text=name)
-    #name=name.lower()
-    print('Lower name:',name)
+
 
 def exchange():
-    #nonlocal name
     code = combobox.get()
     if code:
         try:
-            #response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids=m{name}&vs_currencies=usd")
-            response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
-            #https://api.coingecko.com/api/v3/simple/price?ids=%7Bcrypto_code%7D&vs_currencies=%7Bbase_code%7D
-            #https://api.coingecko.com/api/v3/coins/list
+            response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={name}&vs_currencies=usd")
             response.raise_for_status()
-            print('Статус:',response)
+            data = response.json() #получаем словарь из ключа-значения
 
-            data = response.json()
-            print("data:",data)
-            print('name_after_data',name)
-            #print('Name and Code:', data[0][code])
-            #for key_data, (b, c) in enumerate(data.items()):
-            #tuple_data=data.items()
-            #print('Tuple_data:',tuple_data)
-            if name in data[name.lower().values()]:
-                print(name)
-                exchange_rate = data[name][usd]
-                print(exchange_rate)
-                currency_name = currencies[code]  # currencies.get(code, code)
-                print(currency_name)
-                mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.1f} {currency_name} за 1 доллар")
+            if name.lower() in data.keys():
+                exchange_rate = (data.get(name.lower())).get('usd')
+                currency_name = currencies[code] #получаем название криптовалюты
+                mb.showinfo("Курс обмена криптовалюты", f"Курс {exchange_rate} долларов за один {currency_name}")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена")
-
         except Exception as e:
             mb.showerror("Ошибка", f"Ошибка: {e}")
     else:
@@ -54,18 +37,17 @@ currencies = {
 "ETH": "Ethereum",
 "USDT": "Tether",
 "SOL": "Solana",
-"BNB": "Binance Coin",
-"XRP": "XRP",
 "DOGE": "Dogecoin",
-"USDC": "U.S. Dollar Coin",
 "ADA": "Cardano",
 "TRX": "TRON",
-"USD": "Американский доллар"
+"XLM": "Stellar",
+"pDOTn": "Polkadot",
+"LINK": "Chainlink"
 }
 
 # Создание графического интерфейса
 window = Tk()
-window.title("Курс обмена криптовалюты к доллару")
+window.title("Курс обмена криптовалюты")
 window.geometry("360x180")
 
 Label(text="Выберите код криптовалюты:").pack(padx=10, pady=10)
